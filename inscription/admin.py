4c9724ee7092192bf5da21c6e7b0973f080004cc
Mehran <mehr4nfotovat@gmail.com>
2024-02-13 21:21:48 +0330

    second endpoint created

--- inscription/admin.py
+++ inscription/admin.py
@@ -3,5 +3,19 @@ from .models import WalletAddress, Inscription, NFT
 
 
 admin.site.register(WalletAddress)
-admin.site.register(Inscription)
+
+class NFTInline(admin.TabularInline):  # Using TabularInline for a tabular layout
+    model = NFT
+    can_delete = False  # Prevents deletion of NFT entries
+    readonly_fields = ('nft_address',)  # Makes the fields read-only. Add more fields as needed.
+    extra = 0  # No extra blank forms
+
+    def has_add_permission(self, request, obj=None):
+        # Prevents adding new NFTs from the Inscription admin page
+        return False
+
+class InscriptionAdmin(admin.ModelAdmin):
+    inlines = [NFTInline]
+
+admin.site.register(Inscription, InscriptionAdmin) 
 admin.site.register(NFT)
--- inscription/views.py
+++ inscription/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import WalletAddress, Inscription, NFT
 from .serializers import WalletAddressSerializer, InscriptionSerializer, NFTSerializer
+import json
 
 
 class AddressInscriptionApiView(APIView):
@@ -20,13 +21,31 @@ class AddressInscriptionApiView(APIView):
 class NftInscriptionApiView(APIView):
     def post(self, request):
         wallet_address = request.data.get('wallet_address')
-        nft_addresses = request.data.get('nft_addresses', [])
+        nft_addresses = json.loads(request.data.get('nft_addresses', []))
         print(wallet_address)
         print(nft_addresses)
-        # print(wallet_address)
-        # wallet = WalletAddress.objects.filter(address=wallet_address)
-        # if wallet.exists():
-        #     # print(nft_addresses)
-        #     pass
-        # else:
-        return Response({'address': "no address!"})
+        wallet = WalletAddress.objects.filter(address=wallet_address)
+        if wallet.exists():
+            inscription = Inscription.objects.create(wallet=wallet[0])
+            for nft_address in nft_addresses:
+                try:
+                    NFT.objects.create(nft_address=nft_address, inscription=inscription)
+                    print(f"created {nft_address}")
+                except:
+                    print(f"already exist: {nft_address}")
+            return Response({'address': inscription})
+        else:
+            for nft_address in nft_addresses:
+                nft = NFT.objects.filter(nft_address=nft_address)
+                if nft.exists():
+                    return Response({'Err': "An NFT already exist in DB!"})
+            wallet = WalletAddress.objects.create(address=wallet_address)
+            inscription = Inscription.objects.create(wallet=wallet)
+            for nft_address in nft_addresses:
+                try:
+                    NFT.objects.create(nft_address=nft_address, inscription=inscription)
+                    print(f"created {nft_address}")
+                except:
+                    print(f"already exist: {nft_address}")
+            inscription = InscriptionSerializer(inscription).data
+            return Response({'address': inscription})
